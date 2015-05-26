6151c445d30177b7ffef4521d64764269d08e827
Matt Olson <matt.j.olson@gmail.com>
2015-05-26 16:21:45 -0700

    Implemented deck

--- sandbox/dailyprogrammer/hold-em/card-test.cs
+++ sandbox/dailyprogrammer/hold-em/card-test.cs
@@ -1,5 +1,6 @@
 namespace HoldEm
 {
+    using System;
     using NUnit.Framework;
 
     [TestFixture]
@@ -20,6 +21,34 @@ namespace HoldEm
             var twoclubs = new Card(Suit.Clubs, Value.Two);
 
             Assert.AreEqual("Two of Clubs", twoclubs.ToString());
+        }
+    }
+
+    [TestFixture]
+    public class DeckTest
+    {
+        [Test]
+        public void DeckConstruction()
+        {
+            var deck = new Deck();
+            Assert.AreEqual(deck.cardsLeft(), 52);
+        }
+
+        [Test]
+        public void DeckDrawing()
+        {
+            var deck = new Deck();
+            var drawn = 0;
+            foreach(Suit suit in Enum.GetValues(typeof(Suit))) {
+                foreach(Value val in Enum.GetValues(typeof(Value))) {
+                    var card = deck.draw();
+                    ++drawn;
+
+                    Assert.AreEqual(card.suit, suit);
+                    Assert.AreEqual(card.val, val);
+                    Assert.AreEqual(deck.cardsLeft(), 52-drawn);
+                }
+            }
         }
     }
 }
--- sandbox/dailyprogrammer/hold-em/card.cs
+++ sandbox/dailyprogrammer/hold-em/card.cs
@@ -1,5 +1,8 @@
 namespace HoldEm
 {
+    using System;
+    using System.Collections.Generic;
+
     public enum Suit { Clubs, Diamonds, Hearts, Spades };
     public enum Value { Two,
                         Three,
@@ -29,5 +32,42 @@ namespace HoldEm
         public override string ToString() {
             return val.ToString() + " of " + suit.ToString();
         }
+    }
+
+    public class Deck
+    {
+        public Deck()
+        {
+            deck_ = new List<Card>();
+
+            foreach(Suit suit in Enum.GetValues(typeof(Suit))) {
+                foreach(Value val in Enum.GetValues(typeof(Value))) {
+                    deck_.Add(new Card(suit, val));
+                }
+            }
+        }
+
+        public int cardsLeft() { return deck_.Count; }
+        public Card draw()
+        {
+            Card c = deck_[0];
+            deck_.RemoveAt(0);
+            return c;
+        }
+
+        public void shuffle() // XXX: not unit tested!
+        {
+            Random rng = new Random();
+
+            // Fisher-Yates shuffle
+            for(int i = deck_.Count-1; 0 <= i; i++) {
+                int j = rng.Next(0, i+1);
+                Card c = deck_[i];
+                deck_[i] = deck_[j];
+                deck_[j] = c;
+            }
+        }
+
+        private List<Card> deck_;
     }
 }
