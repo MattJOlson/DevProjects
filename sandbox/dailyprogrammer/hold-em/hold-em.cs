80a8ffd678434ceb5fb7b9b691d6a74f9f6ac3bf
Matt Olson <matt.j.olson@gmail.com>
2015-05-27 00:11:41 -0700

    I guess I should add the main class file, huh

---
+++ sandbox/dailyprogrammer/hold-em/hold-em.cs
@@ -0,0 +1,82 @@
+namespace HoldEm
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static int getNumPlayers()
+        {
+            while (true) {
+                int nPlayers = Convert.ToInt32(Console.ReadLine());
+                if(nPlayers <= 8) return nPlayers;
+                Console.WriteLine("lol, try again");
+            }
+        }
+
+        static List<Player> buildPlayerList(int nPlayers)
+        {
+            List<Player> players = new List<Player>();
+
+            players.Add(new Player()); // user
+            for(int i = 1; i < nPlayers; i++) {
+                players.Add(new Player("CPU" + i.ToString()));
+            }
+
+            return players;
+        }
+
+        static void dealPlayerHands(List<Player> players, Deck deck)
+        {
+            for(int i = 0; i < 2; i++) {
+                foreach(Player p in players) {
+                    p.dealTo(deck.draw());
+                }
+            }
+        }
+
+        static void burnFrom(Deck deck)
+        {
+            Card c = deck.draw();
+            Console.WriteLine("<burned: {0}>", c.ToString());
+        }
+
+        static void deal(string which, int howMany, Deck deck)
+        {
+            burnFrom(deck);
+            Console.Write("{0}: ", which);
+            for(int i = 0; i < howMany; i++) {
+                if(0 < i) { Console.Write(", "); }
+                Card c = deck.draw();
+                Console.Write(c.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many players(2-8)?");
+
+            int nPlayers = getNumPlayers();
+            List<Player> players = buildPlayerList(nPlayers);
+
+            Deck deck = new Deck();
+            deck.shuffle();
+
+            dealPlayerHands(players, deck);
+
+            foreach(Player p in players) {
+                Console.WriteLine(p.ToString());
+            }
+
+
+            deal("Flop", 3, deck);
+
+            deal("Turn", 1, deck);
+
+            deal("River", 1, deck);
+
+            // deck.list();
+        }
+    }
+}
