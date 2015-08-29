6f87c2c8203777a13c199cb991c982967f03a244
Matt Olson <matt.j.olson@gmail.com>
2015-08-28 21:15:20 -0700

    Demand: Refactored to make lambda-predicate demands fully lazy

---
+++ Utils/Demand/Demand/DemandResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Demand
+{
+    public class DemandResult
+    {
+        private Func<bool> _predicate;
+
+        public DemandResult(Func<bool> predicate)
+        {
+            _predicate = predicate;
+        }
+
+        public void Because(string reason)
+        {
+            if (!_predicate()) throw new InvalidOperationException(reason);
+        }
+    }
+}
---
+++ Utils/Demand/Demand/Tests/DemandTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Demand.Tests
+{
+    [TestFixture]
+    public class DemandTests
+    {
+        [Test]
+        public void demanding_a_true_predicate_silently_succeeds()
+        {
+            var trueIsSuccess = Demand.That(() => true);
+
+            Assert.DoesNotThrow(() => trueIsSuccess.Because("We don't throw on success"));
+        }
+
+        [Test]
+        public void demanding_a_false_predicate_throws()
+        {
+            var falseIsFailure = Demand.That(() => false);
+
+            var ex = Assert.Throws<InvalidOperationException>(
+                () => falseIsFailure.Because("We throw on failure")
+            );
+            Assert.That(ex.Message, Is.EqualTo("We throw on failure"));
+        }
+
+        [Test]
+        public void demanding_a_true_expression_succeeds_right_away()
+        {
+            Assert.DoesNotThrow(() => Demand.That(true, "because it's true!"));
+        }
+    }
+}
