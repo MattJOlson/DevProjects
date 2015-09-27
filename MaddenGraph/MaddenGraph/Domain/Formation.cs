eaa051f552d69258723e6176cd282b57d090cd32
Matt Olson <matt.j.olson@gmail.com>
2015-09-26 23:43:07 -0700

    MG-001: Basic weak/strong/backfield eligible-receiver stuff

--- MaddenGraph/MaddenGraph/Domain/Formation.cs
+++ MaddenGraph/MaddenGraph/Domain/Formation.cs
@@ -8,9 +8,19 @@ namespace MaddenGraph.Domain
     {
         public Formation(int weak, int strong)
         {
-            Positions = Enumerable.Range(0, 11);
+            Positions = Enumerable.Range(0, 11).ToList();
+            StrongSideReceivers = Positions.Take(strong).ToList();
+            WeakSideReceivers = Positions.Skip(strong).Take(weak).ToList();
+
+            var backfield = 5 - (strong + weak);
+            BackfieldReceivers = Positions.Skip(strong + weak).Take(backfield).ToList();
         }
 
-        public List<object> Positions { get; }
+        public List<int> Positions { get; }
+        public List<int> EligibleReceivers => WeakSideReceivers.Concat(StrongSideReceivers).Concat(BackfieldReceivers).ToList();
+
+        public List<int> StrongSideReceivers { get; set; }
+        public List<int> WeakSideReceivers { get; }
+        public List<int> BackfieldReceivers { get; set; }
     }
 }
--- MaddenGraph/MaddenGraph/Tests.Unit/Domain/FormationTests.cs
+++ MaddenGraph/MaddenGraph/Tests.Unit/Domain/FormationTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using MaddenGraph.Domain;
+using NUnit.Framework;
 
 namespace MaddenGraph.Tests.Unit.Domain
 {
@@ -11,6 +13,24 @@ namespace MaddenGraph.Tests.Unit.Domain
             var formation = new Formation(2, 2);
 
             formation.Positions.Count.Should().Be(11);
+        }
+
+        [Test]
+        public void default_formations_are_created_with_five_eligible_receivers()
+        {
+            var formation = new Formation(2, 2);
+
+            formation.EligibleReceivers.Count.Should().Be(5);
+        }
+
+        [Test]
+        public void formation_construction_respects_per_side_receiver_params()
+        {
+            var formation = new Formation(2,2);
+
+            formation.WeakSideReceivers.Count.Should().Be(2);
+            formation.StrongSideReceivers.Count.Should().Be(2);
+            formation.BackfieldReceivers.Count.Should().Be(1);
         }
     }
 }
