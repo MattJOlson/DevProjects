56f2a3ecf77494768593e029823ab91bb597eff3
Matt Olson <matt.j.olson@gmail.com>
2015-09-26 21:50:32 -0700

    MG-001: Dirt-simple Formation class

---
+++ MaddenGraph/MaddenGraph/Domain/Formation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaddenGraph.Domain
+{
+    public class Formation
+    {
+        public Formation(int weak, int strong)
+        {
+            Positions = Enumerable.Range(0, 11);
+        }
+
+        public List<object> Positions { get; }
+    }
+}
---
+++ MaddenGraph/MaddenGraph/Tests.Unit/Domain/FormationTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace MaddenGraph.Tests.Unit.Domain
+{
+    [TestFixture]
+    class FormationTests
+    {
+        [Test]
+        public void formations_are_created_with_eleven_player_positions()
+        {
+            var formation = new Formation(2, 2);
+
+            formation.Positions.Count.Should().Be(11);
+        }
+    }
+}
