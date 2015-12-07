2725470e7a9b317d6ac26e6854d133531bd98892
Matt Olson <matt.j.olson@gmail.com>
2015-12-07 12:24:44 -0800

    isSorted works if inlined in its test module, but getting visibility problems picking it up from the Chapter2 module

--- FPinFS/FPinFS.Test/Chapter2Tests.fs
+++ FPinFS/FPinFS.Test/Chapter2Tests.fs
@@ -1,16 +1,28 @@
-﻿namespace FPinFS.Test
+﻿module FPinFS.Test.Chapter2
 
 open NUnit.Framework
 open FsUnit
 open FPinFS.Chapter2
 
-module Chapter2Test =
+[<TestCase(0, 0)>]
+[<TestCase(1, 1)>]
+[<TestCase(2, 1)>]
+[<TestCase(3, 2)>]
+[<TestCase(4, 3)>]
+[<TestCase(6, 8)>]
+let ``fib implements fibonacci sequence``(a, expected) =
+    fib a |> should equal expected
 
-    [<TestCase(0, 0)>]
-    [<TestCase(1, 1)>]
-    [<TestCase(2, 1)>]
-    [<TestCase(3, 2)>]
-    [<TestCase(4, 3)>]
-    [<TestCase(6, 8)>]
-    let ``fib implements fibonacci sequence``(a, expected) =
-        fib a |> should equal expected
+//let rec isSorted f els =
+//    match els with
+//    | [] -> true
+//    | [_] -> true
+//    | a :: b :: cs -> (f a b) && isSorted f (b :: cs)
+
+[<Test>]
+let ``isSorted returns true on an empty list``() =
+    isSorted (<=) [] |> should equal true
+
+[<Test>]
+let ``isSorted detects a sorted list``() =
+    isSorted (<=) [1; 2; 4; 8] |> should equal true
--- FPinFS/FPinFS/Chapter2.fs
+++ FPinFS/FPinFS/Chapter2.fs
@@ -1,8 +1,12 @@
-﻿namespace FPinFS
+﻿module FPinFS.Chapter2
 
-module Chapter2 =
+let rec fib n =
+    if n = 0 then 0
+    else if n = 1 then 1
+    else fib (n-1) + fib(n-2)
 
-    let rec fib n =
-        if n = 0 then 0
-        else if n = 1 then 1
-        else fib (n-1) + fib(n-2)
+let rec isSorted f els =
+    match els with
+    | [] -> true
+    | [_] -> true
+    | a :: b :: cs -> (f a b) && isSorted f (b :: cs)
