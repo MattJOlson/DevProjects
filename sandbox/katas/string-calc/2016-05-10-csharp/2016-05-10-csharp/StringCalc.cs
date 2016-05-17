f17484624cbbe38d02943cb398460f6c9aea4979
Matt Olson <matt.j.olson@gmail.com>
2016-05-17 10:57:26 -0700

    String calc; got bored after newlines/commas

---
+++ sandbox/katas/string-calc/2016-05-10-csharp/2016-05-10-csharp/StringCalc.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace String_calc_2016_05_10_csharp
+{
+    public static class StringCalc
+    {
+        public static int Calc(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            return input.Split(',', '\n')
+                        .Select(int.Parse)
+                        .Aggregate(0, (x, y) => x + y);
+        }
+    }
+}
---
+++ sandbox/katas/string-calc/2016-05-10-csharp/2016-05-10-csharp/StringCalcTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace String_calc_2016_05_10_csharp
+{
+    [TestFixture]
+    public class StringCalcTests
+    {
+        [TestCase("", 0)]
+        [TestCase("0", 0)]
+        [TestCase("1", 1)]
+        public void when_given_at_most_one_input_calc_returns_the_input_integer(string input, int expected)
+        {
+            Check(input, expected);
+        }
+
+        [TestCase("1,1", 2)]
+        [TestCase("1,2,3,4", 10)]
+        public void when_given_many_inputs_calc_returns_their_sum(string input, int expected)
+        {
+            Check(input, expected);
+        }
+
+        [TestCase("1\n1", 2)]
+        [TestCase("1,2\n3\n4", 10)]
+        public void when_given_newlines_as_delims_calc_respects_them(string input, int expected)
+        {
+            Check(input, expected);
+        }
+
+        private void Check(string input, int expected)
+        {
+            StringCalc.Calc(input).Should().Be(expected);
+        }
+    }
+}
